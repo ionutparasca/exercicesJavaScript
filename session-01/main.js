0d5ddcedc6684a2bdae1834d834725a26e6a090b
Admin <florinionutparasca@gmail.com>
2025-03-11 19:11:39 +0100

    Am adaugat inca un exercitiu cu functii si cerinta pentru celelalte exercitii incarcate

--- session-01/main.js
+++ session-01/main.js
@@ -1,13 +1,19 @@
+// Exercițiul 1: Salutare cu nume
+
 function greetings(name) {
   console.log(`Salut, ${name}!`);
 }
 greetings("Ion");
 greetings("Maria");
 
+//Exercițiul 2: Adunare de două numere
+
 function sum(a, b) {
   return `Suma celor doua numere este: ${a + b}!`;
 }
 console.log(sum(3, 5));
+
+//Exercițiul 3: Verificare număr par
 
 function estePar(n) {
   if (n % 2 === 0) {
@@ -19,6 +25,8 @@ function estePar(n) {
 console.log(estePar(4));
 console.log(estePar(5));
 
+//Exercițiul 4: Factorialul unui număr
+
 function factorial(n) {
   if (n === 0) {
     return 1;
@@ -29,6 +37,8 @@ function factorial(n) {
   }
   return result;
 }
+
+//Exercițiul 5: Inversarea unui șir de caractere
 
 console.log(factorial(6));
 
@@ -52,6 +62,7 @@ function inverseazaSir(sir) {
 console.log(inverseazaSir("Ion"));
 console.log(inverseazaSir("Maria"));
 
+//6.Funcție pentru a verifica dacă un număr este prim
 function estePrim(n) {
   if (n <= 1) {
     return `Numarul ${n} nu este prim`;
@@ -69,6 +80,8 @@ function estePrim(n) {
 console.log(estePrim(7));
 console.log(estePrim(10));
 
+//.7Funcție care calculează suma cifrelor unui număr
+
 function sumaCifrelor(numar) {
   let suma = 0;
 
@@ -82,3 +95,14 @@ function sumaCifrelor(numar) {
 
 console.log(sumaCifrelor(1234));
 console.log(sumaCifrelor(5678));
+
+//.8Funcție care returnează cel mai mare dintre două numere
+
+function maxNr(a, b) {
+  if (a < b) {
+    return b;
+  } else {
+    return a;
+  }
+}
+console.log(maxNr(2, 4));
